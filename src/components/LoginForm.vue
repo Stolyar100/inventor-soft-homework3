d8581cc5cbef412f5b67afb6e585e6984c0630b9
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-09-16 10:22:07 +0300

    Create login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+    <label class="login__label default-el"
+      ><input
+        class="login__input"
+        v-model="login"
+        type="text"
+        placeholder="login"
+    /></label>
+    <label class="login__label default-el"
+      ><input
+        class="login__input"
+        v-model="password"
+        type="text"
+        placeholder="login"
+    /></label>
+    <button class="login__btn default-el" @click="loginUser">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      login: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginUser() {},
+  },
+};
+</script>
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+
+  &__btn {
+    background-color: #75c5b7;
+    color: #fff;
+
+    &:active {
+      background-color: #fff;
+    }
+  }
+
+  &__label {
+    margin-bottom: 10px;
+  }
+
+  &__input {
+    border: 0;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="auth">
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  name: "Home",
+  components: {
+    LoginForm,
+  },
+};
+</script>
+<style lang="scss" >
+.auth {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login {
+  max-width: 250px;
+  width: 100%;
+}
+</style>
